45ffb93918bba070b9fda6429ab6722459caf067
David Portillo <david.portillo92@gmail.com>
2023-01-11 17:54:49 -0500

    update webpack props

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -2,14 +2,13 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
   devServer: {
     port: 8081,
-    historyApiFallback: {
-      index: "public/index.html",
-    },
+    historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -18,6 +17,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
